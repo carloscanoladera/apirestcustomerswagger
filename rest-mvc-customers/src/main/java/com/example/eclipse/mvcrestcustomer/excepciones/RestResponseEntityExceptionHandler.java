53ad8f869d47bbde7ae331c60152e7bbe3d3e320
carloscanoladera <carlos.cano.ladera@outlook.es>
2022-03-17 19:45:45 +0100

    v2

---
+++ rest-mvc-customers/src/main/java/com/example/eclipse/mvcrestcustomer/excepciones/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.eclipse.mvcrestcustomer.excepciones;
+
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.example.eclipse.mvcrestcustomer.api.services.ResourceNotFoundException;
+
+
+/**
+ * Created by jt on 10/6/17.
+ */
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request){
+
+        return new ResponseEntity<Object>("Resource Not Found", new HttpHeaders(), HttpStatus.NOT_FOUND);
+
+    }
+}
